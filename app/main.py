e4ced090c1ef1b28d775d961da2a85e5053eb7bc
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-07-18 15:26:12 +0300

    'deleted_double_code'

--- app/main.py
+++ app/main.py
@@ -7,12 +7,7 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{"line": error["line_number"],
-                        "column": error["column_number"],
-                        "message": error["text"],
-                        "name": error["code"],
-                        "source": "flake8"}
-                       for error in errors],
+    return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
             "status": "failed" if errors
             else "passed"}
@@ -20,14 +15,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [{"line": error["line_number"],
-                     "column": error["column_number"],
-                     "message": error["text"],
-                     "name": error["code"],
-                     "source": "flake8"}
-                    for error in value],
-         "path": key,
-         "status": "failed" if value
-         else "passed"}
+        format_single_linter_file(key, value)
         for key, value in linter_report.items()
     ]
